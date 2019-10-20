3b89952c406b8ff1c3e0b40bada66f5bcbf43dd9
Alexandre Bacellar <alexandre.bacellar@rededor.com.br>
2019-10-20 07:44:28 -0300

    Clean code

--- src/shared/rest/rest.service.ts
+++ src/shared/rest/rest.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, HttpService } from '@nestjs/common';
+import { Injectable, HttpService, HttpException, HttpStatus } from '@nestjs/common';
 import { ConfigService } from '../config/config.service';
 import { resolve } from 'url';
-import { first, map } from 'rxjs/operators';
+import { first, map, catchError } from 'rxjs/operators';
 import { AuthBody } from 'src/auth/auth.interfaces';
 import { Enrollment, Verification } from './rest.interfaces';
+import { empty, of } from 'rxjs';
 
 @Injectable()
 export class RestService {
@@ -47,51 +48,65 @@ export class RestService {
     }
 
     postUsers(body: AuthBody) {
-        return this.put(`/users/phone?cpf=${body.cpf}`, body);
+        return this.post(`/users/phone?cpf=${body.cpf}`, body);
     }
 
     putUsers(body: AuthBody) {
-        return this.put('/users/phone', body);
+        return this.put('/users/phone', body).pipe(catchError(() => {
+            throw new HttpException('', HttpStatus.UNAUTHORIZED)
+        }));
     }
 
     private get<T = any>(endpoint: string, query?: any) {
         const url = this.getUrl(endpoint);
         return this.httpService.put(url, query, {
             headers: this.getHeaders()
-        }).pipe(first(), map(res => {
-            console.log('get', endpoint, query, res.data);
-            return res.data as T;
-        }));
+        }).pipe(
+            first(), 
+            map(res => {
+                console.log('get', endpoint, query, res.data);
+                return res.data as T;
+            })
+        );
     }
 
     private post<T = any>(endpoint: string, body?: any) {
         const url = this.getUrl(endpoint);
         return this.httpService.post(url, body, {
             headers: this.getHeaders()
-        }).pipe(first(), map(res => {
-            console.log('post', endpoint, body, res.data);
-            return res.data as T;
-        }));
+        }).pipe(
+            first(), 
+            map(res => {
+                console.log('post', endpoint, body, res.data);
+                return res.data as T;
+            })
+        );
     }
 
     private put<T = any>(endpoint: string, body?: any) {
         const url = this.getUrl(endpoint);
         return this.httpService.put(url, body, {
             headers: this.getHeaders()
-        }).pipe(first(), map(res => {
-            console.log('put', endpoint, body, res.data);
-            return res.data as T;
-        }));
+        }).pipe(
+            first(), 
+            map(res => {
+                console.log('put', endpoint, body, res.data);
+                return res.data as T;
+            })
+        );
     }
 
     private delete<T = any>(endpoint: string) {
         const url = this.getUrl(endpoint);
         return this.httpService.delete(url, {
             headers: this.getHeaders()
-        }).pipe(first(), map(res => {
-            console.log('delete', endpoint, res.data);
-            return res.data as T;
-        }));
+        }).pipe(
+            first(), 
+            map(res => {
+                console.log('delete', endpoint, res.data);
+                return res.data as T;
+            })
+        );
     }
 
     private getUrl(endpoint: string) {
